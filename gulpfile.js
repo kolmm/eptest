3e5d7aeb81e9d13afdefdb5ce37b38e8fa370a55
kolmm <kolmm3@gmail.com>
2017-10-01 15:51:05 +0500

    upd gulp and dot in menu list

--- gulpfile.js
+++ gulpfile.js
@@ -46,12 +46,12 @@ gulp.task("serv", ["style"], function() {
 });
 
 gulp.task("images", function() {
-  return gulp.src("img/**/*.{png,jpg,gif}")
+  return gulp.src("src/img/**/*.{png,jpg,gif}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true})
     ]))
-    .pipe(gulp.dest("img"));
+    .pipe(gulp.dest("src/img"));
 });
 
 gulp.task("symbols", function() {
@@ -66,50 +66,38 @@ gulp.task("symbols", function() {
 
 gulp.task("copy", function() {
   return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
+    "/fonts/**/*.{woff,woff2,otf}",
+    "/img/**",
+    "/js/**",
+    "/*.html"
   ], {
     base: "."
   })
-  .pipe(gulp.dest("../build"));
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("clean", function() {
   return del("build");
 });
 
-gulp.task("build", function(fn) {
-  run(
-    "clean",
-    "copy",
-    "style-build",
-    "images-build",
-    "symbols-build",
-    fn
-  );
-});
 
-gulp.task("style-build", function() {
-  gulp.src("sass/style.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]}),
-      mqpacker({
-        sort: true
-      })
-    ]))
-    .pipe(server.reload({stream: true}))
-    .pipe(gulp.dest("build/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
+gulp.task('build', ['clean', 'images', 'style'], function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+        'src/css/*.css',
+        ])
+    .pipe(gulp.dest('build/css'))
+
+    var buildFonts = gulp.src('src/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('build/fonts'))
+
+    var buildImages = gulp.src('src/img/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('build/img'))
+
+    var buildJs = gulp.src('src/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('build/js'))
+
+    var buildHtml = gulp.src('src/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('build'));
+
 });
